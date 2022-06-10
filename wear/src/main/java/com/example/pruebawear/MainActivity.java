17beccf96b705d4ead4329a83195cbf9ff56f437
Carlos Fernando Soto Zepeda <carlosf.soto1222@gmail.com>
2022-06-09 19:37:51 -0500

    Ejemplo Wear 2do commit

--- wear/src/main/java/com/example/pruebawear/MainActivity.java
+++ wear/src/main/java/com/example/pruebawear/MainActivity.java
@@ -1,15 +1,40 @@
 package com.example.pruebawear;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.example.pruebawear.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
-
-    private TextView mTextView;
+    private Button wBoton = null;
     private ActivityMainBinding binding;
+    private Intent intent;
+    private PendingIntent pendingIntent;
+
+    private NotificationCompat.Builder notification;
+    private NotificationCompat.Builder notification2;
+    private NotificationManagerCompat nm;
+    private NotificationCompat.WearableExtender wearableExtender;
+
+    String idChannel = "Mi_Canal";
+    int idNotification = 001;
+
+    private NotificationCompat.BigTextStyle bigTextStyle;
+
+    String longText = "Without BigStyle, only a single line of text would be visible" +
+            "Any additional text would not appear directly in the notification";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +43,56 @@ public class MainActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mTextView = binding.text;
+        wBoton = findViewById(R.id.wBoton);
+
+        intent = new Intent(
+                MainActivity.this,
+                MainActivity.class
+        );
+
+        nm = NotificationManagerCompat.from(MainActivity.this);
+
+        wearableExtender = new NotificationCompat.WearableExtender();
+
+        bigTextStyle = new NotificationCompat.BigTextStyle().bigText(longText);
+
+        wBoton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                String name = "Notificación";
+
+                NotificationChannel notificationChannel =
+                        new NotificationChannel(idChannel, name, importance);
+
+                nm.createNotificationChannel(notificationChannel);
+
+                pendingIntent = PendingIntent.getActivity(
+                        MainActivity.this,
+                        0,
+                        intent,
+                        0);
+
+                notification = new NotificationCompat.Builder(MainActivity.this, idChannel)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Notificación Wear")
+                        .setContentText(longText)
+                        .setContentIntent(pendingIntent)
+                        .extend(wearableExtender)
+                        .setVibrate(new long[]{10,200,300,400,500,400,300,200,400})
+                        .setStyle(bigTextStyle);
+
+                 notification2 = new NotificationCompat.Builder(MainActivity.this, idChannel)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Mi notificación")
+                        .setContentText("Mi primera notificación Wear")
+                        .extend(wearableExtender);
+
+
+                nm.notify(idNotification, notification.build());
+                nm.notify(idNotification, notification2.build());
+            }
+        });
     }
 }
